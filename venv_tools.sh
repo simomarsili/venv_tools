99213c42fe8ddf334944b686f9709c522cf37830
simomarsili <simo.marsili@gmail.com>
2019-05-29 19:01:40 +0200

    choose python executable

--- venv_tools.sh
+++ venv_tools.sh
@@ -1,90 +1,126 @@
 # Virtual environments utils (via venv module).
-#
+# 
 # Usage:
 # 
-# venv_activate [project]
-# Activate the virtual environment from folder <project> in ~/.venvs.
+# $ venv_activate [-p <python executable>] [project]
+# Activate the virtual environment from folder <project> in $HOME/.venvs.
+# If the folder <project> does not exist, create it and install
+# a ipython kernel with the same name.
 # Default: get project name from the PWD.  
-# If not present, create the folder <project> and install a ipython kernel
-# with the same name.
 # 
-# venv_remove <project>
+# $ venv_remove <project>
 # Remove <project> folder and the associated ipython kernel.
 # 
-# venv_ls
+# $ venv_ls
 # List virtual environments in ~/.venvs.
 
-python=python3
-envs_dir="$HOME/.venvs"
+ENVS_DIR="$HOME/.venvs"
 
 
 function venv_activate {
-    if [ -z $1 ]; then
-	project=${PWD##*/}
+    POSITIONAL=()
+    while [[ $# -gt 0 ]]
+    do
+	key="$1"
+	
+	case $key in
+	    -p|--python)
+		PYTHON="$2"
+		shift # past argument
+		shift # past value
+		;;
+	    *)    # unknown option
+		POSITIONAL+=("$1") # save it in an array for later
+		shift # past argument
+		;;
+	esac
+    done
+    set -- "${POSITIONAL[@]}" # restore positional parameters
+
+    if [ "$#" -eq "0" ]; then
+	PROJECT=${PWD##*/}
+    elif [ "$#" -eq "1" ]; then
+	PROJECT=$1
     else
-	project=$1
+	echo Found: "${POSITIONAL[@]}". Need a single project name.
+	return 1
     fi
 
-    ve=$envs_dir/$project
+    if [ -z $PYTHON ]; then
+	PYTHON=python3
+	echo "default: ", python3
+    else
+	echo "found: "$PYTHON
+    fi
+
+    echo Python: $PYTHON, project: $PROJECT
+
+    ve=$ENVS_DIR/$PROJECT
     if [ -d $ve ]; then
 	source $ve/bin/activate
     else
-	__venv_create $project
+	__venv_create $PROJECT $PYTHON
     fi
 }
 
 
 function venv_remove {
     if [ -z $1 ]; then
-	project=${PWD##*/}
+	PROJECT=${PWD##*/}
     else
-	project=$1
+	PROJECT=$1
     fi
-    rm -r $envs_dir/$project
-    yes | jupyter kernelspec uninstall $project
-    echo "Removed $project project folder from $envs_dir."
+    rm -r $ENVS_DIR/$PROJECT
+    yes | jupyter kernelspec uninstall $PROJECT
+    echo "Removed $PROJECT project folder from $ENVS_DIR."
 }
 
 
 function venv_ls {
-    ls -1 $envs_dir
+    ls -1 $ENVS_DIR
 }
 
 
 function __build_project_ve {
-    project=$1
-    ve=$envs_dir/$project
-    echo "Creating $project project folder in $envs_dir."
-    $python -m venv $ve
+    PROJECT=$1
+    PYTHON=$2
+    ve=$ENVS_DIR/$PROJECT
+    echo "Creating $PROJECT project folder in $ENVS_DIR."
+    # find python version
+    version=`$PYTHON -c "import sys; print(sys.version_info[0])"`
+    if [ "$version" -eq "2" ]; then
+	virtualenv -p $PYTHON $ve
+    elif [ "$version" -eq "3" ]; then
+	$PYTHON -m venv $ve
+    else
+	echo Check Python version
+	return 1
+    fi
     source $ve/bin/activate
     pip install -U pip
     pip install ipykernel
-    ipython kernel install --user --name=$project
+    ipython kernel install --user --name=$PROJECT
 }
 
 
 function __venv_create {
-    if [ -z $1 ]; then
-	project=${PWD##*/}
-    else
-	project=$1
-    fi
-
-    ve=$envs_dir/$project
+    PROJECT=$1
+    PYTHON=$2
+    ve=$ENVS_DIR/$PROJECT
     if [ -d $ve ]; then
-	echo "$project already exists. Replace it? (y/n)"
+	echo "$PROJECT already exists. Replace it? (y/n)"
 	read answer
 	if [ -z "$answer" ]; then return 1; fi
 	if [ $answer == "y" ]; then
 	    rm -rf $ve
-	    __build_project_ve $project
+	    __build_project_ve $PROJECT $PYTHON
 	elif [ $answer == "n" ]; then
 	    source $ve/bin/activate
 	else
 	    echo "Please answer y/n"
 	fi
     else
-	__build_project_ve $project
+	__build_project_ve $PROJECT $PYTHON
     fi
 }
 
